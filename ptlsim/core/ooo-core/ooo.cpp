02276c9b1f6aadbf34d40b47493082f56f96666c
mutien27 <mutien27@gmail.com>
2013-02-07 12:44:08 -0500

    Increase thread idle threshold.

--- ptlsim/core/ooo-core/ooo.cpp
+++ ptlsim/core/ooo-core/ooo.cpp
@@ -863,7 +863,7 @@ bool OooCore::runcycle(void* none) {
         ThreadContext* thread = threads[i];
         if unlikely (!thread->ctx.running) break;
 
-        if unlikely ((sim_cycle - thread->last_commit_at_cycle) > (W64)1024*1024*threadcount) {
+        if unlikely ((sim_cycle - thread->last_commit_at_cycle) > (W64)1024*1024*1024*threadcount) {
             stringbuf sb;
             sb << "[vcpu ", thread->ctx.cpu_index, "] thread ", thread->threadid, ": WARNING: At cycle ",
                sim_cycle, ", ", total_insns_committed,  " user commits: no instructions have committed for ",
